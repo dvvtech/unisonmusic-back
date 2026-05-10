dcb7d0e2b3eabfe07a3eeae259a088dee6bf2cc5
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-05-10 14:43:49 +0300

    add migrator

---
+++ src/Unisonmusic.Migrator/MigrationService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Unisonmusic.Api.DAL;
+
+namespace Unisonmusic.Migrator
+{
+    public class MigrationService(
+        //ILogger<MigrationService> logger,
+        IDbContextFactory<UnisonmusicDbContext> dbContextFactory)
+    {
+        //private readonly ILogger<MigrationService> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+        private readonly IDbContextFactory<UnisonmusicDbContext> _dbContextFactory =
+            dbContextFactory ?? throw new ArgumentNullException(nameof(dbContextFactory));
+
+        public async Task MigrateAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                await using var dbContext = await _dbContextFactory.CreateDbContextAsync(cancellationToken);
+
+                var pendingMigrations = (await dbContext.Database.GetPendingMigrationsAsync(cancellationToken)).ToList();
+                if (pendingMigrations.Any())
+                {
+                    //_logger.LogInformation("Pending migrations found: {PendingMigrationsCount}.", pendingMigrations.Count);
+
+                    foreach (var migration in pendingMigrations)
+                    {
+                        //_logger.LogInformation("Applying migration: {MigrationName}", migration);
+                    }
+
+                    await dbContext.Database.MigrateAsync(cancellationToken);
+                    //_logger.LogInformation("Migrations have been applied successfully.");
+                }
+                else
+                {
+                    //_logger.LogInformation("No pending migrations found. Database is up to date.");
+                }
+            }
+            catch (Exception e)
+            {
+                //_logger.LogCritical(e, "Error occured while migrating database.");
+            }
+        }
+    }
+}
---
+++ src/Unisonmusic.Migrator/Program.cs
@@ -0,0 +1,29 @@
+using Unisonmusic.Api.DAL;
+using Unisonmusic.Migrator;
+
+var environmentName = args.FirstOrDefault() ??
+                              Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ??
+                              throw new InvalidOperationException("ASPNETCORE_ENVIRONMENT in not set");
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile($"appsettings.{environmentName}.json")
+    .Build();
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddSingleton<IConfiguration>(configuration);
+serviceCollection.AddTransient<MigrationService>();
+serviceCollection.AddDAL(configuration);
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+try
+{
+    var migrationService = serviceProvider.GetRequiredService<MigrationService>();
+    await migrationService.MigrateAsync(CancellationToken.None);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Critical error during migration: {ex.Message}");
+    throw;
+}
