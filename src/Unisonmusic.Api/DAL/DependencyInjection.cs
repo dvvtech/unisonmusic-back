ac817baf912d4bad2558d8a64df4a45ee6ab3f28
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-05-25 22:12:59 +0300

    +1

--- src/Unisonmusic.Api/DAL/DependencyInjection.cs
+++ src/Unisonmusic.Api/DAL/DependencyInjection.cs
@@ -26,9 +26,8 @@ namespace Unisonmusic.Api.DAL
                     options.UseNpgsql(cs);
                 }
                 else
-                {
-                    options.UseNpgsql("User ID=postgres;Password=curMal7Hof!;Host=176.108.255.113;Port=5432;Database=unisonmusic");
-                    //options.UseNpgsql(dbOptions.ConnectionString);
+                {                    
+                    options.UseNpgsql(dbOptions.ConnectionString);
                 }
             });
 
